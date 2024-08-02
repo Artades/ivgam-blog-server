e7ca45139cb25755d69617063bf560b2103e196a
Artades <artemijgalaj@gmail.com>
2024-08-02 16:54:54 +0500

    Deploy uploads

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { config } from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as express from 'express';
 import * as path from 'path';
+import * as fs from 'fs'; // Импортируем модуль fs
 import * as cookieParser from 'cookie-parser';
-
 
 config();
 
@@ -17,7 +17,6 @@ async function bootstrap() {
     origin: 'ivgamblogserver.online',
     credentials: true,
   });
-
 
   const config = new DocumentBuilder()
     .setTitle('Ivgam-Blog')
@@ -31,15 +30,22 @@ async function bootstrap() {
     },
   });
 
-
   app.use(cookieParser());
   app.use(
     '/api/uploads',
     express.static(path.join(__dirname, '..', 'uploads')),
   );
 
+  // Проверяем и создаем папку uploads, если она не существует
+  const uploadsPath = path.join(__dirname, '..', 'uploads');
+  if (!fs.existsSync(uploadsPath)) {
+    console.log("Creating Uploads directive...")
+    fs.mkdirSync(uploadsPath, { recursive: true });
+  }
+
   await app.listen(process.env.SERVER_PORT);
 
-  console.log('Port is running on port: ', process.env.SERVER_PORT);
+  console.log('Server is running on port:', process.env.SERVER_PORT);
 }
+
 bootstrap();
